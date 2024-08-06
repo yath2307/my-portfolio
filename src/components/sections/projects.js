7b85840f818571fa15e96b0ff95d608d9026ded8
Yatharth Gupta <yatharth.gupta@mykaarma.com>
2024-08-06 13:18:15 +0530

    fixed issues

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 import { srConfig } from '@config';
@@ -267,10 +267,6 @@ const Projects = () => {
     <StyledProjectsSection>
       <h2 ref={revealTitle}>Other Noteworthy Projects</h2>
 
-      {/*<Link className="inline-link archive-link" to="/archive" ref={revealArchiveLink}>*/}
-      {/*  view the archive*/}
-      {/*</Link>*/}
-
       <ul className="projects-grid">
         {prefersReducedMotion ? (
           <>
@@ -301,10 +297,6 @@ const Projects = () => {
           </TransitionGroup>
         )}
       </ul>
-
-      {/*<button className="more-button" onClick={() => setShowMore(!showMore)}>*/}
-      {/*  Show {showMore ? 'Less' : 'More'}*/}
-      {/*</button>*/}
     </StyledProjectsSection>
   );
 };
